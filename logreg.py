22c358c5646ba654be2575f5188b59ca149eadd0
yoheitaonishi <onishiyoheita@gmail.com>
2016-10-23 12:49:09 -0400

    load_data method imploved

--- logreg.py
+++ logreg.py
@@ -53,3 +53,22 @@ def load_data(dataset):
         urllib.request.urlretrieve(origin, dataset)
 
     print('loading data')
+
+    with gzip.open(dataset, 'rb') as f:
+        try:
+            train_set, valid_set, test_set = pickle.load(f, encoding='latin1')
+        except:
+            train_set, valid_set, test_set = pickle.load(f)
+
+    def shared_dataset(data_xy, borrow=True):
+        data_x, data_y = data_xy
+        shared_x = theano.shared(numpy.asarray(data_x, dtype=theano.config.floatX),borrow=borrow)
+        shared_y = theano.shared(numpy.asarray(data_y, dtype=theano.config.floatX),borrow=borrow)
+        return shared_x, T.cast(shared_y, 'int32')
+
+    test_set_x, test_set_y = shared_dataset(test_set)
+    valid_set_x, valid_set_y = shared_dataset(valid_set)
+    train_set_x, train_set_y = shared_dataset(train_set)
+    rval = [(test_set_x, test_set_y), (valid_set_x, valid_set_y), (train_set_x, traing_set_y)]
+    return rval
+            
