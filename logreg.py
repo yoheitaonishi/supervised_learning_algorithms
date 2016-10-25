52c67e7f69daa3fbf984de26b640ad2548326640
yoheitaonishi <onishiyoheita@gmail.com>
2016-10-25 13:21:12 -0400

    sgd_optimization_mnist method added

--- logreg.py
+++ logreg.py
@@ -52,7 +52,7 @@ def load_data(dataset):
         print('Downloading data from %s' % origin)
         urllib.request.urlretrieve(origin, dataset)
 
-    print('loading data')
+    print('loading data...')
 
     with gzip.open(dataset, 'rb') as f:
         try:
@@ -71,4 +71,82 @@ def load_data(dataset):
     train_set_x, train_set_y = shared_dataset(train_set)
     rval = [(test_set_x, test_set_y), (valid_set_x, valid_set_y), (train_set_x, traing_set_y)]
     return rval
+
+def sgd_optimization_mnist(learning_rate=0.13, n_epochs=1000, dataset='mnist.pkl.gz', batch_size=600):
+    datasets = load_data(dataset)
+
+    train_set_x, train_set_y = datasets[0]
+    valid_set_x, valid_set_y = datasets[1]
+    test_set_x, test_set_y = datasets[2]
+
+    n_train_batches = train_set_x.get_value(borrow=True).shape[0] // batch_size
+    n_valid_batches = valid_set_x.get_value(borrow=True).shape[0] // batch_size
+    n_test_batches = test_set_x.get_value(borrow=True).shape[0] // batch_size
+
+    index = T.lscalar()
+
+    x = T.matrix('x')
+    y = T.ivector('y')
+
+    classifier = LogisticRegression(input=x, n_in=28 * 28, n_out=10)
+
+    cost = classifier.negative_log_likelihood(y)
+
+    test_model = theano.functino(
+        inputs=[index],
+        outputs=classifier.errors(y),
+        givens={
+            x: test_set_x[index * batch_size: (index + 1) * batch_size],
+            y: test_set_y[index * batch_size: (index + 1) * batch_size]
+        }
+    )
+
+    print('training the model...')
+
+    patience = 5000
+    patience_increase = 2
+    
+    improvement_threshold = 0.995
+
+    validation_frequency = min(n_train_batches, patience // 2)
+    best_validation_loss = numpy.inf
+    test_score = 0
+    start_time = timeit.default_time()
+
+    done_looping = False
+    epoch = 0
+    while(epoch < n_epochs) and (not done_looping):
+        epoch = epoch + 1
+        for minibatch_index in range(n_train_batches):
+            minibatch_avg_cost = train_model(minibatch_index)
+            iter = (epoch - 1) * n_train_batches + minibatch_index
+
+            if (iter + 1) % validation_frequency == 0:
+                validation_losses = [validate_model(i) for i in range(n_valid_batches)]
+                this_validation_loss = numpy.mean(validation_lossess)
+
+                print('epoch %i, minibatch %i/%i, validation error %f %%' % (epoch, minibatch_index + 1, n_train_batches, this_validation_loss * 100.))
+
+                if this_validation_loss < best_validation_loss * improvement_threshold:
+                    patience = max(patience, iter * patience_increase)
+
+                    best_validation_loss = this_validation_loss
+
+                    test_losses = [test_model(i) for i in range(n_test_batches)]
+                    test_score = numpy.mean(test_losses)
+
+                    print(('epoch %i, minibatch %i/%i, test error of best model %f %%') % (epoch, minibatch_index + 1, n_train_batches, test_score * 100.))
+
+                    with open('best_model.pkl', 'wb') as f:
+                        pickle.dump(classifier, f)
+
+                if patience <= iter:
+                    done_looping = True
+                    break
+
+    end_time = timeit.default_timer()
+    print(('Optimazation complete with best validation score of %f %%,' 'with test performance %f %%') % (best_validation_loss * 100., test_score * 100.)
+    print('The code run for %d epochs, with %f epoch/sec' % (epoch, 1. * epoch / (end_time - start_time)))
+    print(('The code for file ' + os.path.split(__file__)[1] + ' ran for %.1fs' % ((end_time - start_time))), file=sys.stderr)
+
             
