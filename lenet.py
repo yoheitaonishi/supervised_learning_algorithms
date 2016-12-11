01eeaeb1698b4cc35740143a56509b649b4a83f6
yoheitaonishi <onishiyoheita@gmail.com>
2016-12-11 11:08:51 -0500

    add LeNetConvPoolLayer class

--- lenet.py
+++ lenet.py
@@ -1,33 +1,52 @@
-import theano
-from theano import tensor as T
-from theano.tensor.nnet import conv2d
+from __future__ import print_function
+
+import os
+import sys
+import timeit
 
 import numpy
 
-rng = numpy.random.RandomState(23455)
+import theano
+from theano.tensor as T
+from theano.tensor.signal import pool
+from theano.tensor.nnet import conv2d
 
-# Instantiate 4D tensor for input
-input = T.tensor4(name='input')
-# Instantiate shared value for weights
-w_shp = (2, 3, 9, 9)
-w_bound = numpy.sqrt(3 * 9 * 9)
-W = theano.shared(numpy.asarray(rng.uniform(low=-1.0 / w_bound, high=1.0 / w_bound, size=w_shp), dtype=input.dtype), name='W')
-
-# Biases are usually initialized to zero.
-# However in this paticular application, applying the convolutional Layer to an image without Learning the parameters.
-# therefore initializing them to "simulate" Learning.
-b_shp = (2,)
-b = theano.shared(numpy.asarray(rng.uniform(low=-.5, high=.5, size=b_shp), dtype=input.dtype), name='b')
-
-# build symbolic expression that computes the convolution of input with filters in w
-conv_out = conv2d(input, W)
-
-output = T.nnet.sigmoid(conv_out, b.dimshuffle('x', 0, 'x', 'x'))
-f = theano.function([input], output)
+from logreg import LogisticRegression, load_data
+from mlp import HiddenLayer
 
 
+class LeNetConvPoolLayer(object):
 
+    def __init__(self, rng, input, filter_shape, image_shape, poolsize=(2, 2)):
 
+        # Debug 
+        assert image_shape[1] == filter_shape[1]
+        self.input = input
+
+        fan_in = numpy.prod(filter_shape[1:])
+        fan_out = (filter_shape[0] * numpy.prod(filter_shape[2:]) // numpy.prod(poolside))
+
+        # Initialize weights with random weights
+        W_bound = numpy.sqrt(6. / (fan_in + fan_out))
+        self.W = theano.shared(numpy.asarray(rng.uniform(low=-W_bound, high=W_bound, size=filter_shape), dtype=theano.config.floatX), borrow=True)
+
+        b_values = numpy.zeros((filter_share[0],), dtype=theano.config.floatX)
+        self.b = theano.shared(value=b_values, borrow=True)
+
+        # Convolve input feature maps with filters
+        conv_out = conv2d(input=input, filters=self.W, filter_shape=filter_shape, input_share=image_share)
+        # Pool each feature map individually, using maxpooling
+        pooled_out = pool.pool_2d(input=conv_out, ds=poolsize, ignore_border=True)
+
+        # add the bias term. Since the bias is a vector (1D array), we first reshape it to a tensor of shape (1, n_filters, 1, 1). 
+        # Each bias will thus be broadcasted across mini-batches and feature map width & height
+        self.output = T.tanh(pooled_out + self.b.dimshuffle('x', 0, 'x', 'x'))
+
+        self.params = [self.W, self.b]
+
+        self.input = input
+
+def evaluate_lenet5(learning_rate=0.1, n_epochs=200, dataset='mnist.pkl.gz', nkerns=[20, 50], batch_size=500):
 
 
 
